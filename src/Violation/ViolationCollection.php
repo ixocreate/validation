39398d05d44a3ce9e40a5a6365975ef40497092f
Lucas Seinfeld <l.seinfeld@ixolit.com>
2019-03-01 19:15:38 +0100

    ViolationCollection update to Collection

--- src/Violation/ViolationCollection.php
+++ src/Violation/ViolationCollection.php
@@ -9,17 +9,16 @@ declare(strict_types=1);
 
 namespace Ixocreate\Validation\Violation;
 
-use Ixocreate\Entity\Collection\AbstractCollection;
+use Ixocreate\Collection\AbstractCollection;
 
 final class ViolationCollection extends AbstractCollection
 {
     public function __construct(array $items = [])
     {
-        $items = \array_values($items);
-        $items = (function (Violation ...$violation) {
-            return $violation;
-        })(...$items);
-
-        parent::__construct($items);
+        parent::__construct(
+            (function (Violation ...$violation) {
+                return $violation;
+            })(...$items)
+        );
     }
 }
